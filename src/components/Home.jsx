bc4ef3c6ba238e2fb5052b685dcba895ba871a52
Murilo Bastos <murilocraft5@gmail.com>
2026-03-10 14:43:56 -0300

    Aula 08

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
-const handleClick = (e) => {
-  console.log("Clicou", e);
-};
-const handleClickAgain = (name) => {
-  console.log("Clicou novamente", name);
-};
+import {useState} from "react";
+
 export default function Home() {
+   const [nome , setNome] = useState("Murilo");
+    //let nome = "Murilo";  
+  const handleClick = (e) => {
+    console.log(nome);
+    setNome("Bourached");
+  };
+
   return (
     <div>
       <h2 className="text-main">Nossos Produtos</h2>
       <button onClick={handleClick}>Clique Aqui</button>
-      <button onClick={() => handleClickAgain('Murilo')}>Clique Aqui</button>
+      <p>{nome}</p>
     </div>
   );
 }
