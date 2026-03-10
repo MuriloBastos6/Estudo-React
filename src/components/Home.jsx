e57b92b8f5bca0d520ac9b9b6ce5dd7763c74526
Murilo Bastos <murilocraft5@gmail.com>
2026-03-10 14:21:36 -0300

    Aula 07

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,15 @@
-
+const handleClick = (e) => {
+  console.log("Clicou", e);
+};
+const handleClickAgain = (name) => {
+  console.log("Clicou novamente", name);
+};
 export default function Home() {
-    return (
-        <div>
-            <h2 className="text-main">Nossos Produtos</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-main">Nossos Produtos</h2>
+      <button onClick={handleClick}>Clique Aqui</button>
+      <button onClick={() => handleClickAgain('Murilo')}>Clique Aqui</button>
+    </div>
+  );
 }
-
