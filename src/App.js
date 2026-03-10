3d0855bf72fffe7dbe78b15e4b4ad57e623745a6
Murilo Bastos <murilocraft5@gmail.com>
2026-03-10 12:24:56 -0300

    Aula 06

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 export default function App() {
- const titulo = "Minha primeira aplicação react";
+/* const titulo = "Minha primeira aplicação react";
  const aula = 4;
  const boo = true; // Não é possivel Usar
  const person = {name:"João"}//Não é possivel usar 
- const link = "https://www.google.com.br/"
-
+ const link = "https://github.com/MuriloBastos6/Estudo-React"
+*/
   return (
     <div className="App">
-
-      <div className='content'>{titulo}</div>
-      <p>Aula: {aula}</p>
-      <p>{person.name}</p> {/* Assim pode ser usado*/ }
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        Ir para o Google
-      </a>{/*O link desse jeito pode ser udado*/}
-
-      
-
-
-
-    
+      <Navbar/>
+      <main>
+        <Home/>
+      </main>
     </div>
   );
 }
