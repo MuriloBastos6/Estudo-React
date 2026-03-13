fea3f7345ef9b2d57e890f85122d1082ca13c09c
Murilo Bastos <murilocraft5@gmail.com>
2026-03-13 11:02:03 -0300

    Aula 20

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,26 +6,36 @@ import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 export default function Home() {
   const [produtos, setProdutos] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      
       fetch("http://localhost:8000/Products")
-      .then((res) => res.json())
-      .then((data) => { 
-        setProdutos(data);
-        setIsLoading(false);
-        
-      })
-      
+        .then((res) => res.json())
+        .then((data) => {
+          setProdutos(data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setIsError(err);
+          setIsLoading(false);
+          console.log(err);
+        });
     }, 2000);
   }, []);
 
   return (
     <div>
+      {isError && (
+        <div className="erro-card">
+          <p className="erro-msg">
+            <span>! </span>{isError.message}
+          </p>
+        </div>
+      )}
       {isLoading && (
         <div className="notch-container">
-          <FontAwesomeIcon icon={faCircleNotch} className="loading-notch"/>
+          <FontAwesomeIcon icon={faCircleNotch} className="loading-notch" />
         </div>
       )}
       {produtos && <ProdutosList produtos={produtos} title="Feijões" />}
